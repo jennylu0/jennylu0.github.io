53c6982a4d012af0716357ecd1aa886cb197a439
jennylu0 <jennyluo.jyl@gmail.com>
2026-09-09 14:56:42 -0700

    add gallery images, update gallery items

--- src/assets/data/gallery.js
+++ src/assets/data/gallery.js
@@ -1,8 +1,61 @@
 const imgPath = '/gallery/';
 const data = [
     {
-        img: `${imgPath}Red-head-girls.png`,
+        img: `${imgPath}Re9-grabby.png`,
         filters: ["illustration"],
+    },
+    {
+        img: `${imgPath}Dungeon-crawler-Carl.png`,
+        filters: ["illustration"],
+    },
+    {
+        img: `${imgPath}Isekai.png`,
+        filters: ["illustration"],
+    },
+    {
+        img: `${imgPath}Ryan-goslings-sm.png`,
+        filters: ["merch"],
+        props: ["sticker"],
+        modifier: 'sticker'
+    },
+    {
+        img: `${imgPath}Patterns-formatted-dcc.png`,
+        filters: ["pattern"],
+        props: ["pattern"]
+    },
+    {
+        img: `${imgPath}Patterns-formatted-kuroshitsuji.png`,
+        filters: ["pattern"],
+        props: ["pattern"]
+    },
+    {
+        img: `${imgPath}Murderbot.png`,
+        filters: ["illustration"],
+    },
+    {
+        img: `${imgPath}Patterns-formatted-miku.png`,
+        filters: ["pattern"],
+        props: ["pattern"]
+    },
+    {
+        img: `${imgPath}Patterns-formatted-phm.png`,
+        filters: ["pattern"],
+        props: ["pattern"]
+    },
+    {
+        img: `${imgPath}Leons-sticker2-sm.png`,
+        filters: ["merch"],
+        props: ["sticker"],
+        modifier: 'sticker'
+    },
+    {
+        img: `${imgPath}Phantom-thief-kid.png`,
+        filters: ["illustration"],
+    },
+    {
+        img: `${imgPath}Patterns-formatted-fruit.png`,
+        filters: ["pattern", "original"],
+        props: ["pattern"]
     },
     {
         img: `${imgPath}Fight-like-a-magical-girl.png`,
@@ -18,27 +71,26 @@ const data = [
         props: ["sticker"],
         modifier: 'sticker'
     },
-    {
-        img: `${imgPath}Anime-lucky-cats-sm.png`,
-        filters: ["merch"],
-    },
+    // {
+    //     img: `${imgPath}Anime-lucky-cats-sm.png`,
+    //     filters: ["merch"],
+    // },
     {
         img: `${imgPath}Patterns-formatted-xiao.png`,
         filters: ["pattern"],
         props: ["pattern"]
     },
-    {
-        img: `${imgPath}poke-rainbow.jpeg`,
+     {
+        img: `${imgPath}Red-head-girls.png`,
         filters: ["illustration"],
     },
+    // {
+    //     img: `${imgPath}poke-rainbow.jpeg`,
+    //     filters: ["illustration"],
+    // },
         {
         img: `${imgPath}mew-menu.png`,
         filters: ["illustration"],
-    },
-    {
-        img: `${imgPath}Patterns-formatted-fruit.png`,
-        filters: ["pattern", "original"],
-        props: ["pattern"]
     },
     {
         img: `${imgPath}Patterns-formatted-tmm.png`,
@@ -49,10 +101,10 @@ const data = [
         img: `${imgPath}Spotify-charms.png`,
         filters: ["merch"],
     },
-    {
-        img: `${imgPath}Fruit-stickers.png`,
-        filters: ["merch"],
-    },
+    // {
+    //     img: `${imgPath}Fruit-stickers.png`,
+    //     filters: ["merch"],
+    // },
     // {
     //     img: `${imgPath}mailedit.png`,
     //     filters: ["illustration", "original"],
@@ -75,18 +127,18 @@ const data = [
         props: ["sticker"],
         modifier: 'sticker'
     },
-    {
-        img: `${imgPath}Diona-Klee-sneakers.png`,
-        filters: ["illustration"],
-    },
+    // {
+    //     img: `${imgPath}Diona-Klee-sneakers.png`,
+    //     filters: ["illustration"],
+    // },
     {
         img: `${imgPath}Dungeon-meshi-kitchen-conversion.png`,
-        filters: ["merch", "illustration"],
-    },
-    {
-        img: `${imgPath}kaeya-knight.png`,
         filters: ["illustration"],
     },
+    // {
+    //     img: `${imgPath}kaeya-knight.png`,
+    //     filters: ["illustration"],
+    // },
     {
         img: `${imgPath}Slime-mochi-donut-sm.png`,
         filters: ["merch"],
@@ -102,19 +154,19 @@ const data = [
         filters: ["illustration"],
     },
   
-     {
-        img: `${imgPath}Sticker-sheet-old-consoles.png`,
-        filters: ["merch"],
-    },
+    //  {
+    //     img: `${imgPath}Sticker-sheet-old-consoles.png`,
+    //     filters: ["merch"],
+    // },
     {
         img: `${imgPath}Patterns-formatted-ccs.png`,
         filters: ["pattern"],
         props: ["pattern"]
     },
-    {
-        img: `${imgPath}hualian-ch-224.png`,
-        filters: ["illustration"],
-    },
+    // {
+    //     img: `${imgPath}hualian-ch-224.png`,
+    //     filters: ["illustration"],
+    // },
     // {
     //     img: `${imgPath}wwx-5-star.png`,
     //     filters: ["illustration"],
@@ -124,20 +176,20 @@ const data = [
         filters: ["pattern", "original"],
         props: ["pattern"],
     },
-    {
-        img: `${imgPath}hxh.png`,
-        filters: ["illustration"],
-    },
+    // {
+    //     img: `${imgPath}hxh.png`,
+    //     filters: ["illustration"],
+    // },
     {
         img: `${imgPath}Kumoricon-2024.png`,
         filters: ["illustration", "original"],
     },
-    {
-        img: `${imgPath}Persona5-sticker-sm.png`,
-        filters: ["merch"],
-        props: ["sticker"],
-        modifier: 'sticker'
-    },
+    // {
+    //     img: `${imgPath}Persona5-sticker-sm.png`,
+    //     filters: ["merch"],
+    //     props: ["sticker"],
+    //     modifier: 'sticker'
+    // },
     {
         img: `${imgPath}Patterns-formatted-p5.png`,
         filters: ["pattern"],
@@ -161,18 +213,18 @@ const data = [
         filters: ["pattern"],
         props: ["pattern"]
     },
-    {
-        img: `${imgPath}sticker-sheet-kimetsu.png`,
-        filters: ["merch"],
-    },
-    {
-        img: `${imgPath}strawberry-kaeluc.png`,
-        filters: ["illustration"],
-    },
-    {
-        img: `${imgPath}Sticker-sheet-stamps.png`,
-        filters: ["merch"],
-    }
+    // {
+    //     img: `${imgPath}sticker-sheet-kimetsu.png`,
+    //     filters: ["merch"],
+    // },
+    // {
+    //     img: `${imgPath}strawberry-kaeluc.png`,
+    //     filters: ["illustration"],
+    // },
+    // {
+    //     img: `${imgPath}Sticker-sheet-stamps.png`,
+    //     filters: ["merch"],
+    // }
 ];
 
 export default data;
