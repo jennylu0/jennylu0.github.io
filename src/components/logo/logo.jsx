b77c6aaf1ddfc55ca3eee2b766e483a0bc046d51
jennylu0 <jennyluo.jyl@gmail.com>
2026-09-09 15:36:02 -0700

    responsive fixes, link logo

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -4,9 +4,11 @@ import logo from '../../assets/images/logo.png';
 function Logo() {
   return (
       <div className="logo">
+        <a href="/" className="logo__link">
           <div className="logo__container">
             <img src={logo} alt="logo" className="logo__img" />
           </div>
+        </a>
       </div>
   )
 }
